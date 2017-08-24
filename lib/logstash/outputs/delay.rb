19ae9de17916000eae94831307fc23367485404b
Tristan Roussel <tristan.roussel@epita.fr>
2017-08-24 10:37:25 +0200

    Remove the use of an openstruct to use two arrays (one with the event and one with the time).

--- lib/logstash/outputs/delay.rb
+++ lib/logstash/outputs/delay.rb
@@ -7,96 +7,111 @@ require "rufus-scheduler"
 
 # An delay output that does nothing.
 class LogStash::Outputs::Delay < LogStash::Outputs::Base
-  config_name "delay"
-  concurrency :single
-  
-  config :delay, :validate => :number, :default => 5, :required => false
-  config :out, :validate => :string, :default => "stdout", :required => false
-  config :hosts, :validate => :uri, :default => "//127.0.0.1", :required => false
-  config :index, :validate => :string, :default => "logstash", :required => false
-  config :document_id, :validate => :string, :required => false
-  config :action, :validate => :string, :default => "index", :required => false
-  config :doc_as_upsert, :validate => :boolean, :default => false, :required => false
-  
-  @events
-  @output_plugin
+    config_name "delay"
+    concurrency :single
 
-  private
-  def createElasticsearchConfig
-	config = {
-		"hosts" => @hosts,
-		"index" => @index,
-		"action" => @action,
-		"document_id" => @document_id,
-		"doc_as_upsert" => @doc_as_upsert
-	}
-	return config
-  end
+    config :delay, :validate => :number, :default => 5, :required => false
+    config :out, :validate => :string, :default => "stdout", :required => false
+    config :hosts, :validate => :uri, :default => "//127.0.0.1", :required => false
+    config :index, :validate => :string, :default => "logstash", :required => false
+    config :document_id, :validate => :string, :required => false
+    config :action, :validate => :string, :default => "index", :required => false
+    config :doc_as_upsert, :validate => :boolean, :default => false, :required => false
 
-  private
-  def encodeEvent(event)
-	[[event, event]]
-  end
+    @events
+    @times
+    @debug
+    @output_plugin
 
-  private
-  def redirectMessageToPlugin(event_buffer)
-	if @out == "stdout"
-		event_buffer.each do |event|
-			@output_plugin.multi_receive_encoded(encodeEvent(event))
-			puts ""
-		end
-	elsif @out == "elasticsearch"
-		@output_plugin.multi_receive(event_buffer)
-	else
-		puts = "Choose between stdout or elasticsearch"
-	end
-	return nil
-  end
+    private
+    def createElasticsearchConfig
+        config = {
+            "hosts" => @hosts,
+            "index" => @index,
+            "action" => @action,
+            "document_id" => @document_id,
+            "doc_as_upsert" => @doc_as_upsert
+        }
+        return config
+    end
 
-  private
-  def chooseOutputPlugin
-	if @out == "stdout"
-		@output_plugin = LogStash::Outputs::Stdout.new
-	elsif @out == "elasticsearch"
-		@output_plugin = LogStash::Outputs::ElasticSearch.new(createElasticsearchConfig())
-	else
-		puts "Choose between stdout or elasticsearch"
-	end
-	return nil
-  end
+    private
+    def encodeEvent(event)
+        [[event, event]]
+    end
 
-  public
-  def register
-	@events = []
-	@output_plugin = nil
-	chooseOutputPlugin()
+    private
+    def redirectMessageToPlugin(event_buffer)
+        if @out == "stdout"
+            event_buffer.each do |event|
+                @output_plugin.multi_receive_encoded(encodeEvent(event))
+                puts ""
+            end
+        elsif @out == "elasticsearch"
+            index = 0
+            batch_size = 10000
+            while index < event_buffer.length do
+                if index + batch_size >= event_buffer.length
+                    @output_plugin.multi_receive(event_buffer[index..event_buffer.length])
+                else
+                    @output_plugin.multi_receive(event_buffer[index, batch_size])
+                end
+                index += batch_size
+            end
+        else
+            puts = "Choose between stdout or elasticsearch"
+        end
+        return nil
+    end
 
-	@output_plugin.register
+    private
+    def chooseOutputPlugin
+        if @out == "stdout"
+            @output_plugin = LogStash::Outputs::Stdout.new
+        elsif @out == "elasticsearch"
+            @output_plugin = LogStash::Outputs::ElasticSearch.new(createElasticsearchConfig())
+        else
+            puts "Choose between stdout or elasticsearch"
+        end
+        return nil
+    end
 
-	scheduler = Rufus::Scheduler.new
+    public
+    def register
+        @events = []
+        @times = []
+        @debug = File.open("logstash-output-delay.debug", 'w')
+        @output_plugin = nil
 
-	scheduler_delay = @delay.to_s << "s"
-	scheduler.every scheduler_delay do
-		if @events.length != 0
-			now = Time.new
-			index = 0
-			@events.detect {|event|
-				index += 1
-				event.time > now
-			}
-			redirectMessageToPlugin(@events.slice!(0, index).map {|e| e.event})
-		end
-	end
-  end # def register
+        chooseOutputPlugin
 
-  public
-  def receive(event)
-	expended = OpenStruct.new
-	expended.event = event
-	expended.time = Time.new + @delay
+        @output_plugin.register
 
-	@events << expended
+        scheduler = Rufus::Scheduler.new
 
-    return "Event received"
-  end # def event
+        scheduler_delay = @delay.to_s << "s"
+
+        scheduler.every scheduler_delay do
+            if @times.length != 0
+                now = Time.new
+                index = 0
+                @times.detect {|time|
+                    index += 1
+                    time > now
+                }
+                redirectMessageToPlugin(@events.slice!(0, index))
+                @times.slice!(0, index)
+            end
+        end
+    end # def register
+
+    public
+    def receive(event)
+        time = Time.new + @delay
+
+        @events << event
+        @times << time
+
+        return "Event received"
+    end # def event
 end # class LogStash::Outputs::Delay
